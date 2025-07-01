10492201ad8c3d2aeaa2fae74a92f2dc0df31581
mfg637 <mfg636@gmail.com>
2025-07-01 13:34:12 +0300

    add sigma scale node

--- sigmas/__init__.py
+++ sigmas/__init__.py
@@ -1,6 +1,15 @@
-from . import concat_sigmas, invert_sigmas, offset_sigmas, split_by_value, power, logarithm
+from . import (
+    scale_to_range,
+    concat_sigmas,
+    invert_sigmas,
+    offset_sigmas,
+    split_by_value,
+    power,
+    logarithm
+)
 
 NODE_CLASS_MAPPINGS = {
+    "ScaleToRange": scale_to_range.StaleToRange,
     "InvertSigmas": invert_sigmas.InvertSigmas,
     "ConcatSigmas": concat_sigmas.ConcatSigmas,
     "OffsetSigmas": offset_sigmas.OffsetSigmas,
